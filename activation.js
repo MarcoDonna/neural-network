a3ca5ca8e6a4b6d789de2de8032824817db5a1e8
MarcoDonna <marco.donna03@gmail.com>
2023-05-05 16:20:44 +0200

    created most common activation functions

---
+++ activation.js
@@ -0,0 +1,31 @@
+function sigmoid(x){
+    return 1 / (1 + Math.exp(-x));
+}
+
+function sigmoidPrime(x){
+    return sigmoid(x) * (1 - sigmoid(x));
+}
+
+function tanh(x){
+    return Math.tanh(x);
+}
+
+function tanhPrime(x){
+    return 1 - Math.pow(Math.tanh(x), 2);
+}
+
+function relu(x){
+    return x < 0 ? 0 : x;
+}
+
+function reluPrime(x){
+    return x < 0 ? 0 : 1;
+}
+
+function leakyRelu(x){
+    return x < 0 ? 0.002 * x : x;
+}
+
+function leakyRelyPrime(x){
+    return x < 0 ? 0.002 : 1;
+}
