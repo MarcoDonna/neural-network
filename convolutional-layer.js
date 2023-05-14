aeb113fd810a226560da56e075cea4ae105e3c76
MarcoDonna <marco.donna03@gmail.com>
2023-05-14 11:25:47 +0200

    convolutional layer forward pass

--- convolutional-layer.js
+++ convolutional-layer.js
@@ -1,5 +1,8 @@
 class ConvolutionalLayer{
     constructor(inputWidth, inputHeigth, activationFunction, activationFunctionPrime, config={}){
+        this.inputWidth = inputWidth;
+        this.inputHeigth = inputHeigth;
+        
         this.activationFunction = activationFunction;
         this.activationFunctionPrime = activationFunctionPrime;
 
@@ -8,8 +11,8 @@ class ConvolutionalLayer{
         this.filter.stride = config.stride || 1;
         //this.flatten = config.flatten || true;
 
-        this.filter.numberHeigth = (inputHeigth - this.filter.size) / this.filter.stride + 1;
-        this.filter.numberWidth = (inputWidth - this.filter.size) / this.filter.stride + 1;
+        this.filter.numberHeigth = (this.inputWidth - this.filter.size) / this.filter.stride + 1;
+        this.filter.numberWidth = (this.inputHeigth - this.filter.size) / this.filter.stride + 1;
         this.filter.number = this.filter.numberHeigth * this.filter.numberWidth;
     
         this.initRandomBiases();
@@ -47,5 +50,35 @@ class ConvolutionalLayer{
                 }
             }
         }
+    }
+
+    forward(prevLayer){
+        if(!prevLayer.output || prevLayer.output.length != this.inputHeigth || prevLayer.output[0].length != this.inputWidth)
+            throw new Error('Invalid input');
+
+        this.inputs = prevLayer.output;
+
+        this.activation = [];
+        this.outputs = [];
+
+        for(let filterRowIndex = 0; filterRowIndex < this.filter.numberHeigth; filterRowIndex++){
+            this.activation.push([]);
+            this.outputs.push([]);
+            for(let filterColIndex = 0; filterColIndex < this.filter.numberWidth; filterColIndex++){
+                //Apply filter to input, store result in activation and output
+                let activation = this.biases[filterRowIndex][filterColIndex];
+
+                const filterStartRow = filterRowIndex * this.filter.stride;
+                const filterStartCol = filterColIndex * this.filter.stride;
+
+                for(let i = 0; i < this.filter.size; i++){
+                    const inputsSubset = this.inputs[filterStartRow].slice(filterStartCol, filterStartCol + this.filter.size);
+                    activation += dot(inputsSubset, this.weights[filterRowIndex][filterColIndex][i]);
+                }                
+
+                this.activation[filterRowIndex].push(activation);
+                this.outputs[filterRowIndex].push(this.activationFunction(activation));
+            }
+        }        
     }
 }
