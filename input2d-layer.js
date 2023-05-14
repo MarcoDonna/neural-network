4dd9f873371038f155a116d03ee4283d8b2a5ae2
MarcoDonna <marco.donna03@gmail.com>
2023-05-14 11:25:30 +0200

    2d input layer

---
+++ input2d-layer.js
@@ -0,0 +1,16 @@
+class Input2DLayer{
+    constructor(heigth, width){
+        this.width = width;
+        this.heigth = heigth;
+    }
+
+    get output(){
+        return this.outputs;
+    }
+
+    forward(inputs){
+        if(inputs.length != this.heigth || inputs[0].length != this.width)
+            throw new Error('Invalid input size');
+        this.outputs = inputs;
+    }
+}
